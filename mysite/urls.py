754f74f86fab95ba5e082b9914f8f15db79aa798
Michael Hancock <michaelhancock89@gmail.com>
2016-05-28 15:10:04 -0700

    Finished extensions

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import django.contrib.auth.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login),
 ]
